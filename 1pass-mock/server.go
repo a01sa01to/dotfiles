d305247a88dba3ed4293265124e3cf51ee24a6a0
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-09-23 13:00:50 +0900

    feat: 1pass-mock for CI

---
+++ 1pass-mock/server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func vaultsHandler(c echo.Context) error {
+	data, err := os.ReadFile("data/vaults.json")
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to read vaults.json")
+	}
+	return c.JSONBlob(http.StatusOK, data)
+}
+
+func itemHandler(c echo.Context) error {
+	vaultID := c.Param("vault_id")
+	itemID := c.Param("item_id")
+	log.Printf("Received request for vault_id: %s, item_id: %s", vaultID, itemID)
+
+	data, err := os.ReadFile(fmt.Sprintf("data/%s/%s.json", vaultID, itemID))
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to read item.json")
+	}
+	return c.JSONBlob(http.StatusOK, data)
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.GET("/v1/vaults", vaultsHandler)
+	e.GET("/v1/vaults/:vault_id/items/:item_id", itemHandler)
+
+	port := ":8080"
+	e.Logger.Infof("Starting server on port %s", port)
+	if err := e.Start(port); err != nil {
+		e.Logger.Fatal(err)
+	}
+}
