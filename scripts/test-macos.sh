b0421b45ad70c3b2a1b91b00d525ec375499c60d
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-09-25 18:24:19 +0900

    update tests

--- scripts/test-macos.sh
+++ scripts/test-macos.sh
@@ -43,6 +43,10 @@ FileExists "$CONFIG_DIR/.npmrc"
 FileContains "$CONFIG_DIR/.npmrc" "//registry.npmjs.org/:_authToken=test-npm-access-token"
 FileContains "$CONFIG_DIR/.npmrc" "//npm.pkg.github.com/:_authToken=test-github-packages-pat"
 
+FileNotExists "$CONFIG_DIR/.bashrc"
+
+FileExists "$CONFIG_DIR/.zshrc"
+
 # --------------------------------------------------- #
 
 if [ $FAIL -eq 1 ]; then
